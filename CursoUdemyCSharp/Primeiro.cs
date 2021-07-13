737c953e5fda3ca511ff022309a93b253cacf5b5
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-07-13 11:49:46 -0300

    Sainda de dados simples em C#

--- CursoUdemyCSharp/Primeiro.cs
+++ CursoUdemyCSharp/Primeiro.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 
 namespace Primeiro
 {
@@ -7,6 +8,21 @@ namespace Primeiro
         static void Main(string[] args)
         {
             Console.WriteLine("Ola Mundo");
+
+            double x = 10.35784;
+
+            Console.Write("Número completo: ");
+            Console.WriteLine(x); //Imprime o número completo
+            Console.Write("Número com 2 casas decimais: ");
+            Console.WriteLine(x.ToString("F2")); //Imprime o número com 2 casas decimais
+            Console.Write("Número com 4 casas decimais: ");
+            Console.WriteLine(x.ToString("F4")); //Imprime o número com 4 casas decimais
+            Console.Write("Número sem o tipo de moeda local: ");
+            //Para usar o CultureInfo.InvariantCulture é preciso importar o (System.Globalization;).
+            Console.WriteLine(x.ToString("F2", CultureInfo.InvariantCulture));
+
+
+            Console.ReadLine();
         }
     }
 }
