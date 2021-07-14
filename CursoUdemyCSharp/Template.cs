4977d3626c08e03c0d239da292977d20b9b43f41
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-07-14 02:31:23 -0300

    Template

---
+++ CursoUdemyCSharp/Template.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
