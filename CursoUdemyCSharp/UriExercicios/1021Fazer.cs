c5fe16954ff25c147156db18227072ff6cfe3833
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-07-16 14:07:27 -0300

    URI Exercicios Desafio para fazer

---
+++ CursoUdemyCSharp/UriExercicios/1021Fazer.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace Template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double N, cem, cinquenta, vinte, dez, cinco, dois, um, resto, valorLido;
+
+            N = double.Parse(Console.ReadLine());
+            valorLido = N;
+            cem = N / 100;
+            resto = N % 100;
+
+            cinquenta = resto / 50;
+            N = resto % 50;
+            vinte = N / 20;
+            resto = N % 20;
+            dez = resto / 10;
+            cinco = resto / 5;
+            N = resto % 5;
+            dois = N / 2;
+            resto = N % 2;
+            um = resto / 1;
+
+            Console.WriteLine("NOTAS:");
+            Console.WriteLine(cem + " nota(s) de R$ 100.00");
+            /*
+            Console.WriteLine(cinquenta + " nota(s) de R$ 50,00");
+            Console.WriteLine(vinte + " nota(s) de R$ 20,00");
+            Console.WriteLine(dez + " nota(s) de R$ 10,00");
+            Console.WriteLine(cinco + " nota(s) de R$ 5,00");
+            Console.WriteLine(dois + " nota(s) de R$ 2,00");
+            Console.WriteLine(um + " nota(s) de R$ 1,00");
+            */
+        }
+    }
+}
