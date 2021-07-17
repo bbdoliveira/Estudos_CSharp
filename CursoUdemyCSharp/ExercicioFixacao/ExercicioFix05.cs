1d4b29193ce1e74593e74e0b09d415c568f5c0c4
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-07-16 23:28:37 -0300

    ExercicioFix05 feito

--- CursoUdemyCSharp/ExercicioFixacao/ExercicioFix05.cs
+++ CursoUdemyCSharp/ExercicioFixacao/ExercicioFix05.cs
@@ -7,7 +7,22 @@ namespace ExercicioFix02
     {
         static void Main(string[] args)
         {
-            
+            double nota1, nota2, notaFinal;
+
+            nota1 = double.Parse(Console.ReadLine());
+            nota2 = double.Parse(Console.ReadLine());
+
+            notaFinal = nota1 + nota2;
+
+            if (notaFinal >= 60.0)
+            {
+                Console.WriteLine("NOTA FINAL = " + notaFinal.ToString(("F1"), CultureInfo.InvariantCulture));
+            }
+            else
+            {
+                Console.WriteLine("NOTA FINAL = " + notaFinal.ToString(("F1"), CultureInfo.InvariantCulture));
+                Console.WriteLine("REPROVADO");
+            }
         }
     }
 }
