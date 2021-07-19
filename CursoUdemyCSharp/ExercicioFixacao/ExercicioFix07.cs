b6d399141bdcff8c1171850829993e1c90de8bd2
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-07-18 23:10:54 -0300

    ExercicioFix07

---
+++ CursoUdemyCSharp/ExercicioFixacao/ExercicioFix07.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Globalization;
+
+namespace ExercicioFix02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double media, idade, soma;
+            int count;
+
+            idade = double.Parse(Console.ReadLine());
+            soma = 0.0;
+            count = 0;
+
+            if (idade < 0.0)
+            {
+                Console.WriteLine("Impossivel calcular");
+            }
+            else
+            {
+                while (idade >= 0)
+                {
+                    soma += idade;
+                    count++;
+                    idade = double.Parse(Console.ReadLine());
+                }
+                media = soma / count;
+                Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
+            }
+
+
+        }
+    }
+}
