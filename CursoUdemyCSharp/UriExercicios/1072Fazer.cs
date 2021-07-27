e5c50f8964182ca8c629e19fc66bdf5661aa63ff
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-07-26 22:03:23 -0300

    URI Exercicio 1072 por fazer

---
+++ CursoUdemyCSharp/UriExercicios/1072Fazer.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+
+            x = int.Parse(Console.ReadLine());
+
+            
+        }
+    }
+}
