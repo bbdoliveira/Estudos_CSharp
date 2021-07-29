6447bf1ef7df7da3a973a6813c88ef6a90101028
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-07-28 23:54:55 -0300

    URI Exercicio 1072

--- CursoUdemyCSharp/UriExercicios/1072Fazer.cs
+++ CursoUdemyCSharp/UriExercicios/1072Fazer.cs
@@ -6,11 +6,28 @@ namespace Template
     {
         static void Main(string[] args)
         {
-            int x;
+            int x, caso, dentro, fora;
+
+            caso = 0;
+            dentro = 0;
+            fora = 0;
 
             x = int.Parse(Console.ReadLine());
 
-            
+            for (int i = 0; i < x; i++)
+            {
+                caso = int.Parse(Console.ReadLine());
+                if (caso >= 10 && caso <= 20)
+                {
+                    dentro += 1;
+                }
+                else
+                {
+                    fora += 1;
+                }
+            }
+            Console.WriteLine(dentro + "in");
+            Console.WriteLine(fora + "out");
         }
     }
 }
