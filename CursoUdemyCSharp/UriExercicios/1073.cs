158f105184807a7babdb3306ff58e7a83e4bb8d9
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-07-29 00:27:28 -0300

    URI Exercicio 1073

--- CursoUdemyCSharp/UriExercicios/1073.cs
+++ CursoUdemyCSharp/UriExercicios/1073.cs
@@ -6,7 +6,25 @@ namespace Template
     {
         static void Main(string[] args)
         {
-            
+            int n, count;
+            double par, resultado;
+
+            count = 0;
+            par = 2;
+            resultado = 0;
+
+            n = int.Parse(Console.ReadLine());
+
+            if (n % 2 == 0)
+            {
+                for (int i = 0; count < n; i++)
+                {
+                    resultado = Math.Pow(par, 2.0);
+                    count += 2;
+                    Console.WriteLine(par + "^2" + " = " + resultado);
+                    par += 2;
+                }
+            }
         }
     }
 }
