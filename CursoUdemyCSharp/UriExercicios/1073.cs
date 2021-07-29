5994469b1ba3d035a95404b60a9020fce0152bf9
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-07-28 23:58:58 -0300

    1073 Começando

---
+++ CursoUdemyCSharp/UriExercicios/1073.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace Template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, caso, dentro, fora;
+
+            caso = 0;
+            dentro = 0;
+            fora = 0;
+
+            x = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < x; i++)
+            {
+                caso = int.Parse(Console.ReadLine());
+                if (caso >= 10 && caso <= 20)
+                {
+                    dentro += 1;
+                }
+                else
+                {
+                    fora += 1;
+                }
+            }
+            Console.WriteLine(dentro + " in");
+            Console.WriteLine(fora + " out");
+        }
+    }
+}
