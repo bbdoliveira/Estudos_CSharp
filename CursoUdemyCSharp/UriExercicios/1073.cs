91375b747fa68e31a207c474580b1fbf4ee87d54
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-07-28 23:59:28 -0300

    URI 1073

--- CursoUdemyCSharp/UriExercicios/1073.cs
+++ CursoUdemyCSharp/UriExercicios/1073.cs
@@ -6,28 +6,7 @@ namespace Template
     {
         static void Main(string[] args)
         {
-            int x, caso, dentro, fora;
-
-            caso = 0;
-            dentro = 0;
-            fora = 0;
-
-            x = int.Parse(Console.ReadLine());
-
-            for (int i = 0; i < x; i++)
-            {
-                caso = int.Parse(Console.ReadLine());
-                if (caso >= 10 && caso <= 20)
-                {
-                    dentro += 1;
-                }
-                else
-                {
-                    fora += 1;
-                }
-            }
-            Console.WriteLine(dentro + " in");
-            Console.WriteLine(fora + " out");
+            
         }
     }
 }
