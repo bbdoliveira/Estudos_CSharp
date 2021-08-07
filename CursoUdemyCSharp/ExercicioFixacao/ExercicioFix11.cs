5c9876c42e43aa4a9f34456078658dec9797b674
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-08-07 15:33:29 -0300

    ExercicioFix11

---
+++ CursoUdemyCSharp/ExercicioFixacao/ExercicioFix11.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Globalization;
+
+namespace ExercicioFix10
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            double[] vet;
+            double soma = 0.0, media;
+
+            N = int.Parse(Console.ReadLine());
+            vet = new double[N];
+
+            string[] s = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < N; i++)//Loop de armazenamento dos valores no vetor
+            {
+                vet[i] = double.Parse(s[i]);
+            }
+
+            for (int i = 0; i < N; i++)//Loop de impressão dos valores
+            {
+                Console.Write(vet[i].ToString("F1", CultureInfo.InvariantCulture) + " ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < N; i++)//Loop para soma dos valores
+            {
+                soma += vet[i];
+            }
+            Console.WriteLine(soma.ToString("F2", CultureInfo.InvariantCulture));
+
+            media = soma / N;
+            Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
