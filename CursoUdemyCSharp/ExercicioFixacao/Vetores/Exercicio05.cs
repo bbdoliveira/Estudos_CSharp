d9f202413cb1fcb34ecc6d98f08e03458cfe808d
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-08-08 19:57:45 -0300

    Exercicio Vetores 05

---
+++ CursoUdemyCSharp/ExercicioFixacao/Vetores/Exercicio05.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Globalization;
+
+namespace Exercicio04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, soma = 0, count = 0;
+            double media = 0.0;
+
+            N = int.Parse(Console.ReadLine());
+
+            int[] numerosInt = new int[N];
+
+            string[] v = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < N; i++)
+            {
+                numerosInt[i] = int.Parse(v[i]);
+
+                if (numerosInt[i] % 2 == 0)
+                {
+                    soma += numerosInt[i];
+                    count++;
+                }
+            }
+
+            media = (double) soma / count;
+            Console.WriteLine(media.ToString("F1", CultureInfo.InvariantCulture));
+        }
+    }
+}
