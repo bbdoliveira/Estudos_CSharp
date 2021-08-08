ba2ba3279624a4a50a587ebd926d2614dea0debb
Bruno Bonifácio de Oliveira <bbdoliveira@gmail.com>
2021-08-08 17:55:28 -0300

    Exercicio Vetores 01

---
+++ CursoUdemyCSharp/ExercicioFixacao/Vetores/Exercicio01.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Globalization;
+
+namespace Template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, indice = 0;
+            double maior = 0.0;
+
+            N = int.Parse(Console.ReadLine());
+
+            double[] numeros = new double[N];
+
+            string[] v = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < N; i++)
+            {
+                numeros[i] = double.Parse(v[i]);
+
+                if (numeros[i] > maior )
+                {
+                    maior = numeros[i];
+                    indice = i;
+                }
+            }
+
+            Console.WriteLine(maior.ToString("F1", CultureInfo.InvariantCulture));
+            Console.WriteLine(indice);
+        }
+    }
+}
